b7e23299ccfff73bff1625f9b488943f03f5da02
alexgitrepo <alex.dev4web.com>
2020-02-21 18:25:35 +0500

    Add jsx to register page

--- src/neko-2-features/f-1-authorization/a-2-register/r-1-ui/Register.tsx
+++ src/neko-2-features/f-1-authorization/a-2-register/r-1-ui/Register.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import {SIGN_IN_PATH} from "../../../../neko-1-main/m-1-ui/Routes";
+import style from'./Register.module.css'
 
 interface IRegisterProps {
 
@@ -14,9 +15,13 @@ const Register: React.FC<IRegisterProps> = (
 
 
     return (
-        <div>
+        <div className={style.register}>
             register
-
+            <input type="text" placeholder="enter e-mail"/>
+            <input type="password" placeholder="enter password" />
+            <input type="password" placeholder="repeat password"/>
+            <button>Register</button>
+            <NavLink to={SIGN_IN_PATH}>sign in</NavLink>
 
         </div>
     );
--- src/neko-2-features/f-1-authorization/a-2-register/r-3-dal/RegisterAPI.ts
+++ src/neko-2-features/f-1-authorization/a-2-register/r-3-dal/RegisterAPI.ts
@@ -1,9 +1,12 @@
 import {instance} from "../../../../base-url";
 
+
 export interface IRegisterData {
 
 }
 
 export const RegisterAPI = {
-
+sendRegistrationRequest:(email:string,password:string)=>{
+    return instance.post(`auth/register`,{email,password})
+}
 };
