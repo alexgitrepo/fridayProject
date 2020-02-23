5194b1504bde04f5c0fc2f622429a000750bc2fa
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-02-23 20:16:00 +0300

    change action in signIn branch

--- src/neko-2-features/f-1-authorization/a-1-sign-in/s-2-bll/signInThunk.ts
+++ src/neko-2-features/f-1-authorization/a-1-sign-in/s-2-bll/signInThunk.ts
@@ -1,6 +1,6 @@
 import {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {IAppStore} from "../../../../neko-1-main/m-2-bll/store";
-import {ISignInActions} from "./b-2-redux/signInActions";
+import {SignInAllActionsType} from "./b-2-redux/signInActions";
 import {SignInAPI} from "../s-3-dal/SignInAPI";
 import {passwordCoding} from "../../../f-2-helpers/h-1-authorization/passwordCoding";
 import {INekoActions, nekoSetName} from "../../../f-4-social/s-1-neko/n-2-bll/b-2-redux/nekoActions";
@@ -9,11 +9,11 @@ type Return = void;
 type ExtraArgument = {};
 type IGetStore = () => IAppStore;
 
-export const signIn =
+export const signInThunk =
     (email: string, password: string, rememberMe: boolean)
-        : ThunkAction<Return, IAppStore, ExtraArgument, ISignInActions> =>
+        : ThunkAction<Return, IAppStore, ExtraArgument, SignInAllActionsType> =>
         async (
-            dispatch: ThunkDispatch<IAppStore, ExtraArgument, ISignInActions | INekoActions>,
+            dispatch: ThunkDispatch<IAppStore, ExtraArgument, SignInAllActionsType | INekoActions>,
             getStore: IGetStore
         ) => {
             try {
